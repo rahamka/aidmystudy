eaabef1d2e90f23451f41bf89c032db48c520e30
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-05-05 18:02:05 +0500

    fixed vite.config.js file.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Base must match the GitHub repository name
-  base: "/aidmystudy/",
-  server: {
-    hmr: {
-      overlay: false,
-    },
-  },
+  base: "/aidmystudy/", // Ye aapki repository ka naam hai
 });
